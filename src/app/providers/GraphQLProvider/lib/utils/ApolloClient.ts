656e9d4569b2eca1879c34f7340203885f084de6
Sergey Gorbatenko <siarheimjxfull@gmail.com>
2025-11-06 22:01:48 +0300

    fix(apollo):new version

--- src/app/providers/GraphQLProvider/lib/utils/ApolloClient.ts
+++ src/app/providers/GraphQLProvider/lib/utils/ApolloClient.ts
@@ -1,8 +1,12 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 import { API_URL_COUNTRIES } from '../constants';
 
-export const CustomApolloClient = new ApolloClient({
+const httpLink = new HttpLink({
   uri: API_URL_COUNTRIES,
+});
+
+export const CustomApolloClient = new ApolloClient({
+  link: httpLink,
   cache: new InMemoryCache(),
 });
